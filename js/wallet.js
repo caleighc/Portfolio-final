2625ade868c777c830340511ec1d90facbe159fc
caleighc <caleighc@umich.edu>
2022-04-12 15:05:08 -0400

    change

--- js/wallet.js
+++ js/wallet.js
@@ -1,25 +1,33 @@
-import Web3 from 'web3';
-var button = document.querySelector('#send');
+// var Web3 = require('web3')
+// var web3 = new Web3('http://localhost:8545')
+var Web3 = require("web3");
+console.log(Web3.version);
+
+
+var button = document.querySelector('#send')
 button.onclick = async function() {
     console.log("Connect wallet")
-    if (window.ethereum){
-        // const Web3 = require("web3")
-                let w3 = new Web3(window.ethereum);
-                const accs = await w3.eth.getAccounts()
-                const act = accs[0]
-                await w3.eth.sendTransaction({
-                    from: act,
-                    to: '0x165CD37b4C644C2921454429E7F9358d18A45e14',
-                    value: "10000000000000000"
-                })
-                console.log("Donation complete")
-                document.getElementById("send").innerHTML = "Thank you!"
-                return
+    if (window.ethereum) {
+        const Web3 = require("web3")
+        let w3 = new Web3(window.ethereum)
+        const accs = await w3.eth.getAccounts()
+        const act = accs[0]
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await w3.eth.sendTransaction({
+            from: act,
+            to: '0x165CD37b4C644C2921454429E7F9358d18A45e14',
+            value: "10000000000000000"
+        })
+        console.log("Donation complete")
+        document.getElementById("send").innerHTML = "Thank you!"
+        return
 
-            } else {
-                alert("Please download metamask from https://metamask.io")
-            }
-};
+        } else {
+            console.error("Install metamask")
+            alert("Please download metamask from https;//metamask.io")
+        }
+}
+
 
 
 
