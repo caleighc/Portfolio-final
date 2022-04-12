4a1c09ac2432b5d8bba4bfcf226fdd5ea1b11884
caleighc <caleighc@umich.edu>
2022-04-12 15:52:35 -0400

    change

--- js/wallet.js
+++ js/wallet.js
@@ -1,29 +1,50 @@
 // var Web3 = require('web3')
 // var web3 = new Web3('http://localhost:8545')
 
-var button = document.querySelector('#send')
+// var button = document.querySelector('#send')
+// button.onclick = async function() {
+//     console.log("Connect wallet")
+//     if (window.ethereum) {
+//         // const Web3 = require("web3")
+//         let w3 = new Web3(window.ethereum)
+//         const accs = await w3.eth.getAccounts()
+//         const act = accs[0]
+//         const provider = new ethers.providers.Web3Provider(window.ethereum)
+//         await w3.eth.sendTransaction({
+//             from: act,
+//             to: '0x165CD37b4C644C2921454429E7F9358d18A45e14',
+//             value: "10000000000000000"
+//         })
+//         console.log("Donation complete")
+//         document.getElementById("send").innerHTML = "Thank you!"
+//         return
+
+//         } else {
+//             console.error("Install metamask")
+//             alert("Please download metamask from https;//metamask.io")
+//         }
+// }
+
+var button = document.querySelector('#send');
 button.onclick = async function() {
     console.log("Connect wallet")
-    if (window.ethereum) {
+    if (window.ethereum){
         // const Web3 = require("web3")
-        let w3 = new Web3(window.ethereum)
-        const accs = await w3.eth.getAccounts()
-        const act = accs[0]
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        await w3.eth.sendTransaction({
-            from: act,
-            to: '0x165CD37b4C644C2921454429E7F9358d18A45e14',
-            value: "10000000000000000"
-        })
-        console.log("Donation complete")
-        document.getElementById("send").innerHTML = "Thank you!"
-        return
-
-        } else {
-            console.error("Install metamask")
-            alert("Please download metamask from https;//metamask.io")
-        }
-}
+                    var web3 = new Web3(Web3.givenProvider);
+        const accs = await web3.eth.getAccounts()
+                    const act = accs[0]
+        await web3.eth.sendTransaction({
+                        from: act,
+                        to: '0x165CD37b4C644C2921454429E7F9358d18A45e14',
+                        value: "10000000000000000"
+                    })
+                console.log("Donation complete")
+                document.getElementById("send").innerHTML = "Thank you!"
+                return
+                } else {
+                    alert("Please download metamask from https://metamask.io")
+                }
+};
 
 
 
